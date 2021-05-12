f3dec517d54be09bfdab6aa3a8ebc945d1e8dfbb
Koz Ross <koz.ross@retro-freedom.nz>
2021-05-12 12:48:44 +1200

    Add FromJSON generic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for large-records
 
+## 0.1.0.1 -- 2021-05-12
+
+* Add `gparseJSON` to `Generic.JSON` module
+
 ## 0.1.0.1 -- 2021-05-10
 
 * Add `Generic.Arbitrary` module with `garbitrary` implementation
--- src/Data/Record/Generic/JSON.hs
+++ src/Data/Record/Generic/JSON.hs
@@ -1,26 +1,78 @@
-{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- | Generic conversion to/from JSON
-module Data.Record.Generic.JSON (
-    gtoJSON
-  ) where
+module Data.Record.Generic.JSON
+  ( gtoJSON,
+    gparseJSON,
+  )
+where
 
-import Data.Aeson
-import Data.Proxy
-
-import qualified Data.Text as Text
-
+import Data.Aeson.Types
+  ( FromJSON,
+    Parser,
+    ToJSON (toJSON),
+    Value,
+    object,
+    withObject,
+    (.:),
+  )
+import Data.HashMap.Strict (HashMap)
+import Data.Kind (Type)
+import Data.Proxy (Proxy (Proxy))
 import Data.Record.Generic
+  ( Constraints,
+    Dict (Dict),
+    Generic (dict, from, metadata, to),
+    I (I),
+    K (K),
+    Metadata (Metadata),
+    Rep (Rep),
+    mapKKK,
+    recordFieldNames,
+    recordName,
+    unI,
+  )
 import qualified Data.Record.Generic.Rep as Rep
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import GHC.Exts (Any)
 
-gtoJSON :: forall a. (Generic a, Constraints a ToJSON) => a -> Value
+gtoJSON ::
+  forall (a :: Type).
+  (Generic a, Constraints a ToJSON) =>
+  a ->
+  Value
 gtoJSON =
-      object
+  object
     . Rep.collapse
     . Rep.zipWith (mapKKK $ \n x -> (Text.pack n, x)) recordFieldNames
     . Rep.cmap (Proxy @ToJSON) (K . toJSON . unI)
     . from
   where
-    Metadata{..} = metadata (Proxy @a)
+    Metadata {..} = metadata (Proxy @a)
+
+gparseJSON ::
+  forall (a :: Type).
+  (Generic a, Constraints a FromJSON) =>
+  Value ->
+  Parser a
+gparseJSON = withObject (recordName md) go
+  where
+    md :: Metadata a
+    md = metadata @a Proxy
+    constraints :: Vector (Dict FromJSON Any)
+    Rep constraints = dict @a $ Proxy @FromJSON
+    fields :: Vector (K String Any)
+    Rep fields = recordFieldNames @a md
+    go :: HashMap Text Value -> Parser a
+    go obj = to . Rep <$> (traverse (decode obj) . V.zip fields $ constraints)
+    decode ::
+      HashMap Text Value ->
+      (K String Any, Dict FromJSON Any) ->
+      Parser (I Any)
+    decode obj (K k, Dict) = I <$> obj .: Text.pack k
